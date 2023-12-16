264ea2f90d76de404164b46fb06fe42f149bebdb
Noi <noidor2019@gmail.com>
2023-12-16 21:26:20 +0700

    Added tg credentials using user-secrets

---
+++ telegram-lotte-bot/Handlers/CommandsHandler.cs
@@ -0,0 +1,11 @@
+﻿
+namespace telegram_lotte_bot.Handlers
+{
+    internal class CommandsHandler
+    {
+        public void PrintText()
+        {
+
+        }
+    }
+}
---
+++ telegram-lotte-bot/TelegramCredentials.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.UserSecrets;
+
+namespace telegram_lotte_bot
+{
+    public class TelegramCredentials
+    {
+        private IConfiguration configuration;
+
+        public TelegramCredentials()
+        {
+            var builder = new ConfigurationBuilder().AddUserSecrets("bf684e1b-a247-4665-b47f-2e87843a7b49");
+
+            configuration = builder.Build();
+        }
+
+        public string GetBotToken()
+        {
+            return configuration.GetSection("BotToken").Value;
+        }
+
+        public string GetChatId()
+        {
+            return configuration.GetSection("ChatId").Value;
+        }
+    }
+}

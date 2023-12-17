a5f9223b6fdfc89afc9411f7dfce2da4de9270e5
Noi <noidor2019@gmail.com>
2023-12-17 13:58:21 +0700

    bot CommandsHandler with httpClient, first iteration

--- telegram-lotte-bot/Handlers/CommandsHandler.cs
+++ telegram-lotte-bot/Handlers/CommandsHandler.cs
@@ -1,10 +1,46 @@
 ﻿
+using System.Net;
+using static System.Net.Mime.MediaTypeNames;
+
 namespace telegram_lotte_bot.Handlers
 {
-    internal class CommandsHandler
+    public class CommandsHandler
     {
-        public void PrintText()
+        // Чисто сахар что бы каждый раз не писать _credentials.GetBotToken()
+        private string BotToken { get { return _credentials.GetBotToken(); } }
+
+        private string ChatId { get { return _credentials.GetChatId(); } } // Поменять на динамически-получаемый id
+
+        private readonly TelegramCredentials _credentials;
+
+        private static readonly HttpClient _httpClient = new HttpClient(new SocketsHttpHandler
         {
+            PooledConnectionLifetime = TimeSpan.FromHours(1)
+        })
+        {
+            BaseAddress = new Uri(@$"https://api.telegram.org")
+        };
+
+        public CommandsHandler(TelegramCredentials credentials)
+        {
+            _credentials = credentials;
+        }
+
+        public async Task SendMessage(string text)
+        {
+            string apiEndpoint = $"/bot{BotToken}/sendMessage";
+
+            var content = new StringContent($"chat_id={ChatId}&text={Uri.EscapeDataString(text)}", System.Text.Encoding.UTF8, "application/x-www-form-urlencoded");
+            HttpResponseMessage response = await _httpClient.PostAsync(apiEndpoint, content);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                await Console.Out.WriteLineAsync(await response.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                Console.Out.WriteLine("Нет ответа.");
+            }
 
         }
     }
--- telegram-lotte-bot/Program.cs
+++ telegram-lotte-bot/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace telegram_lotte_bot
+﻿using telegram_lotte_bot.Handlers;
+
+namespace telegram_lotte_bot
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Started.");
+
+            TelegramCredentials credentials = new();
+            CommandsHandler commandsHandler = new(credentials);
+
+            await commandsHandler.SendMessage("test");
         }
     }
 }
